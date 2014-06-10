ca22f57b78835d78478e0ed1984064f3a41d2fbe
Carolyn <carolyn82416@gmail.com>
2014-06-10 22:48:59 +0800

    f9

--- AllAboutFood/script.js
+++ AllAboutFood/script.js
@@ -11,105 +11,82 @@ window.fbAsyncInit = function () {
         version    : 'v2.0'
     });
 
-    
-    
-//FB.getLoginStatus(function(response) {
-//  if (response.status === 'connected') {
-//    
-//  
-//  } else if (response.status === 'not_authorized') {
-//      
-//   
-//  } else {
-//
-//  }
-//
-//});
-    
-// This is called with the results from from FB.getLoginStatus().
-  function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      testAPI();
-    } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
-        
-         //要求使用者登入，索取publish_actions權限
-      console.log("this user is not authorizied your apps");
-      FB.login(function (response) {
-          if (response.authResponse) { 
-              window.location.reload();
-          };
-      }, {scope: 'user_about_me,email,user_location,user_photos,publish_actions,user_birthday,user_likes'});
+    FB.getLoginStatus(function (response) {
+        if (response.status === 'connected') {
 
-    } else {
-      // The person is not logged into Facebook, so we're not sure if
-      // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
-        
-            //同樣要求使用者登入
-      console.log("this isn't logged in to Facebook.");
-      FB.login(function (response) {
-          if (response.authResponse) {
-              window.location.reload();
-          } else {
-          }
-      });
-    }
-  }
+            var uid = response.authResponse.userID;
+            var accessToken = response.authResponse.accessToken;
+            FB.api('/me', function (response) {
+                //console.log(response);
+                $("body").append('My links is' + response.link);
+                 $("body").append('My Username is' + response.username); document.getElementsByTagName('body').innerHTML = ""
+                 $("body").append('My ID is' + response.id);
+            });
 
-  // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
-  function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
+
+            FB.ui({
+                method: 'share',
+                href: 'http://chialinyu.github.io/G9/AllAboutFood/index/index.html',
+            }, function (response) {});
+
+
+
+
+            FB.ui({
+                method: 'send',
+                link: 'http://chialinyu.github.io/G9/AllAboutFood/index/index.html',
+            });
+
+            FB.api('/me/likes', function (response) {
+                console.log(response)
+                for (var i = 0; i < response.data.length; i++){
+                    console.log(response.data[i].name);
+                    }
+            });
+
+            FB.api('/me/picture?type=normal', function(response) { // normal/large/squere 
+                var str="<img src="+ response.data.url +">";
+                $('body').append(str);
+            });
+
+
+
+            FB.api('/me/photos', 'post', {
+                name:"test",
+                message: 'this is parse photo',
+                url: "http://chialinyu.github.io/G9/AllAboutFood/index/images/logo3.jpg"//如果要init運行只能用絕對絕對路徑
+            }, function (response) {
+                if (!response || response.error) {
+                    alert('Error occured:' + response);
+                    console.log(response);
+                } else {
+                    alert('Post ID: ' + response.id);
+                }
+            });
+
+        } else if (response.status === 'not_authorized') {
+            console.log("this user is not authorizied your apps");
+            FB.login(function (response) {
+                // FB.api('/me/feed', 'post', {message: 'I\'m started using FB API'});
+                if (response.authResponse) { // if user login to your apps right after handle an event
+                    window.location.reload();
+                };
+            }, {
+                scope: 'user_about_me,email,user_location,user_photos,publish_actions,user_birthday,user_likes'
+            });
+        } else {
+            console.log("this isn't logged in to Facebook.");
+            FB.login(function (response) {
+                if (response.authResponse) {
+                    window.location.reload();
+                } else {
+                    //alertify.alert('An Error has Occurs,Please Reload your Pages');
+                }
+            });
+        }
     });
-  }
+}; //<<<<<<<<<<<<<<<init end    
 
- 
-
-  // Now that we've initialized the JavaScript SDK, we call 
-  // FB.getLoginStatus().  This function gets the state of the
-  // person visiting this page and can return one of three states to
-  // the callback you provide.  They can be:
-  //
-  // 1. Logged into your app ('connected')
-  // 2. Logged into Facebook, but not your app ('not_authorized')
-  // 3. Not logged into Facebook and can't tell if they are logged into
-  //    your app or not.
-  //
-  // These three cases are handled in the callback function.
-
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-
-  };
-
-
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
-  function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
-    });
-  }    
-
-
-}; //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<init end
 
 
 
