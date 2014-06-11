fd3961f42522898e7aaefe16bab31bb266249ed8
Carolyn <carolyn82416@gmail.com>
2014-06-11 23:42:30 +0800

    hi36

--- AllAboutFood/script.js
+++ AllAboutFood/script.js
@@ -37,7 +37,7 @@ window.fbAsyncInit = function () {
 }; //<<<<<<<<<<<<<<<init end    
 
     console.log("I am out!!");
-$('#fb_login').click(function(){
+function clickFBLogin(){
     console.log("I am in!!");
     FB.getLoginStatus(function (response) {
         if (response.status === 'connected') {
@@ -66,7 +66,7 @@ $('#fb_login').click(function(){
             FacebookLogin();
         }
     });
-})
+}
 
 //LOGIN Fix
 
