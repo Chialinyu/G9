07c38fb940ca49e060d5bfe1a425708d47a504ba
Carolyn <carolyn82416@gmail.com>
2014-06-15 23:59:38 +0800

    f150

--- AllAboutFood/script.js
+++ AllAboutFood/script.js
@@ -131,7 +131,7 @@ function FacebookLogin() {
                               
                               document.cookie= userID;
                               console.log("login~id=",userID);
-//                              $("#UserName").append('<p >'+"Hi~ "+userName+"</p>");
+                              $("#UserName").append('<p >'+"Hi~ "+userName+"</p>");
                               
 //                              var currentuser = new Parse.Query(FacebookID);
 //                              currentuser.equalTo("username", "currentuser");
