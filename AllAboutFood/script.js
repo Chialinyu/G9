892e0417dc54ab19cb854db458e590e37883201d
Carolyn <carolyn82416@gmail.com>
2014-06-15 20:48:01 +0800

    f122

--- AllAboutFood/script.js
+++ AllAboutFood/script.js
@@ -195,6 +195,7 @@ function deleteAllCookies() {
 }(document, 'script', 'facebook-jssdk'));
 
 function testshow(){
-$("#UserName").after('<p>'+'哈哈哈哈哈'+'</p>');
+$("#UserName").append('<p>'+'哈哈哈哈哈'+'</p>');
+    
     console.log("Hi");
 }
